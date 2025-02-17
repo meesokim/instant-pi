f61f80b11901aa15c40da1536086d855c14e1dac
Miso Kim <meesokim@gmail.com>
2025-02-17 23:14:52 +0900

    instant-pi-1b/br_instantpi1b_defconfig instant-pi-1b/build-image.sh instant-pi-1b/genimage.cfg

--- instant-pi-1b/build-image.sh
+++ instant-pi-1b/build-image.sh
@@ -5,10 +5,10 @@ if [ "$#" -eq 0 ] || [ "$1" != "-N" ] && [ "$1" != "-L" ]; then
     if [ ! -d buildroot ]; then
         git clone https://github.com/buildroot/buildroot --depth 1
     fi
-    wget -N https://github.com/raspberrypi/firmware/raw/master/boot/bcm2708-rpi-b.dtb -P buildroot/output/images/
-    wget -N https://github.com/raspberrypi/firmware/raw/master/boot/bootcode.bin -P buildroot/output/images/
-    wget -N https://github.com/raspberrypi/firmware/raw/master/boot/start_cd.elf -P buildroot/output/images/
-    wget -N https://github.com/raspberrypi/firmware/raw/master/boot/fixup_cd.dat -P buildroot/output/images/
+    wget -N --no-if-modified-since https://github.com/raspberrypi/firmware/raw/master/boot/bcm2708-rpi-b.dtb -P buildroot/output/images/
+    wget -N --no-if-modified-since https://github.com/raspberrypi/firmware/raw/master/boot/bootcode.bin -P buildroot/output/images/
+    wget -N --no-if-modified-since https://github.com/raspberrypi/firmware/raw/master/boot/start_cd.elf -P buildroot/output/images/
+    wget -N --no-if-modified-since https://github.com/raspberrypi/firmware/raw/master/boot/fixup_cd.dat -P buildroot/output/images/
 fi
 
 make -j $(nproc) -C buildroot defconfig BR2_DEFCONFIG=../br_instantpi1b_defconfig && \
